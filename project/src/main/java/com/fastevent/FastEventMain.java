7922a7abd1d1430b782da306afdc0040dafeac4e
BR7FORLIFE <diazpachecobrayan@gmail.com>
2025-03-18 15:59:38 -0500

    feat: implement PrincipalInterface with CSS styling and layout adjustments

--- project/src/main/java/com/fastevent/FastEventMain.java
+++ project/src/main/java/com/fastevent/FastEventMain.java
@@ -1,10 +1,12 @@
 package com.fastevent;
 
-import com.fastevent.views.SplashScreen;
+import com.fastevent.views.PrincipalInterface;
+//import com.fastevent.views.SplashScreen;
 
 public class FastEventMain {
     public static void main(String[] args) {
         //lanzamiento de la aplicacion
-        SplashScreen.launch(SplashScreen.class, args);
+        //SplashScreen.launch(SplashScreen.class, args);
+        PrincipalInterface.launch(PrincipalInterface.class, args);
     }
 }
--- project/src/main/java/com/fastevent/views/components/CssRoutes.java
+++ project/src/main/java/com/fastevent/views/components/CssRoutes.java
@@ -4,4 +4,5 @@ public class CssRoutes {
     public String splashScreenCss = getClass().getResource("/css/SplashSreen.css").toExternalForm();
     public String register_loginCss = getClass().getResource("/css/Register_login.css").toExternalForm();
 
+    public String PrincipalInterface = getClass().getResource("/css/PrincipalInterface.css").toExternalForm();
 }
