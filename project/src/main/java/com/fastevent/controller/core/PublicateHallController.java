ef125bc326469a6971098d6cf1f9513ea170c1a3
BR7FORLIFE <diazpachecobrayan@gmail.com>
2025-04-26 21:30:17 -0500

    Implement hall publication functionality with enhanced input validation and UI updates; modify user and publication data structures

--- project/src/main/java/com/fastevent/controller/core/PublicateHallController.java
+++ project/src/main/java/com/fastevent/controller/core/PublicateHallController.java
@@ -1,5 +1,74 @@
 package com.fastevent.controller.core;
 
+import java.io.FileReader;
+import java.io.FileWriter;
+
+import com.fastevent.common.constants.PathConst;
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
+import com.google.gson.JsonArray;
+import com.google.gson.JsonObject;
+
+import javafx.scene.control.TextField;
+
 public class PublicateHallController {
-    
+    private static PathConst pathConst = new PathConst();
+
+    public static void comprobateInformation(TextField... infoToHall) {
+        try {
+            String nameOfHall = infoToHall[0].getText();
+            String descriptionOfHall = infoToHall[1].getText();
+            String ubicationOfHall = infoToHall[2].getText();
+            int capacityOfHall = Integer.parseInt(infoToHall[3].getText());
+            int dimensionOfHall = Integer.parseInt(infoToHall[4].getText());
+            long cellphone = Long.parseLong(infoToHall[5].getText());
+            float priceOfHall = Float.parseFloat(infoToHall[6].getText());
+
+            for (TextField field : infoToHall) {
+                field.setDisable(true);
+            }
+
+            saveHall(nameOfHall, descriptionOfHall, ubicationOfHall, capacityOfHall, dimensionOfHall, cellphone,
+                    priceOfHall);
+
+            System.out.println("Salón agregado exitosamente!");
+
+        } catch (Exception e) {
+            System.out.println(e.getMessage());
+        }
+    }
+
+    private static void saveHall(Object... informationTohall) {
+        Gson gson = new GsonBuilder().setPrettyPrinting().create();
+        JsonArray publicationArray = new JsonArray();
+        JsonObject root = new JsonObject();
+
+        try (FileReader reader = new FileReader(pathConst.getPublicationJson())) {
+            root = gson.fromJson(reader, JsonObject.class);
+            publicationArray = root.getAsJsonArray("publication");
+            System.out.println(publicationArray);
+        } catch (Exception e) {
+            System.out.println(e.getMessage());
+        }
+
+        JsonObject newPublication = new JsonObject();
+        newPublication.addProperty("name", (String) informationTohall[0]);
+        newPublication.addProperty("description", (String) informationTohall[1]);
+        newPublication.addProperty("ubication", (String) informationTohall[2]);
+        newPublication.addProperty("capacity", (int) informationTohall[3]);
+        newPublication.addProperty("dimension", (int) informationTohall[4]);
+        newPublication.addProperty("cellphone", (long) informationTohall[5]);
+        newPublication.addProperty("price", (float) informationTohall[6]);
+        newPublication.addProperty("valoration", Math.random() * 6.0);
+
+        publicationArray.add(newPublication);
+
+        root.add("publication", publicationArray);
+
+        try (FileWriter writer = new FileWriter(pathConst.getPublicationJson())) {
+            gson.toJson(root, writer);
+        } catch (Exception e) {
+            System.out.println(e.getMessage());
+        }
+    }
 }
--- project/src/main/java/com/fastevent/views/CoreInterface/PublicationOfHallIU.java
+++ project/src/main/java/com/fastevent/views/CoreInterface/PublicationOfHallIU.java
@@ -2,6 +2,7 @@ package com.fastevent.views.CoreInterface;
 
 import com.fastevent.common.constants.PathConst;
 import com.fastevent.common.constants.StylesConst;
+import com.fastevent.controller.core.PublicateHallController;
 
 import javafx.geometry.Insets;
 import javafx.geometry.Pos;
@@ -18,101 +19,114 @@ import javafx.scene.paint.Color;
 
 //esta es la vista a la hora de subir un salon de eventos
 public class PublicationOfHallIU {
-    private static PathConst pathConst = new PathConst();
+        private static PathConst pathConst = new PathConst();
 
-    public static HBox publicateHall() {
-        Image image = new Image(pathConst.getPlusImage());
-        ImageView imageView = new ImageView(image);
+        public static HBox publicateHall() {
+                Image image = new Image(pathConst.getPlusImage());
+                ImageView imageView = new ImageView(image);
 
-        // labels del layout
-        Label nameOfHallLabel = new Label("Nombre del salón: ");
-        Label ubicationOfHallLabel = new Label("Ubicación del salón: ");
-        Label priceOfHallLabel = new Label("Precio del salón: ");
-        Label capacityOfHallLabel = new Label("Aforo del salón: ");
-        Label dimensionOfHallLabel = new Label("Dimensiones del salón: ");
+                // labels del layout
+                Label nameOfHallLabel = new Label("Nombre del salón: ");
+                Label descriptionOfHallLabel = new Label("Descripción del salón: ");
+                Label ubicationOfHallLabel = new Label("Ubicación del salón: ");
+                Label capacityOfHallLabel = new Label("Aforo del salón: ");
+                Label dimensionOfHallLabel = new Label("Dimensiones del salón: ");
+                Label cellphoneOfHallLabel = new Label("Telefono: ");
+                Label priceOfHallLabel = new Label("Precio del salón: ");
 
-        // estilos de los labels
-        for (Label label : new Label[] {
-                nameOfHallLabel, ubicationOfHallLabel, priceOfHallLabel, capacityOfHallLabel, dimensionOfHallLabel
-        }) {
-            label.setStyle(StylesConst.getStyleForLabels());
+                // estilos de los labels
+                for (Label label : new Label[] {
+                                nameOfHallLabel, descriptionOfHallLabel, ubicationOfHallLabel, capacityOfHallLabel, dimensionOfHallLabel, cellphoneOfHallLabel, priceOfHallLabel
+                }) {
+                        label.setStyle(StylesConst.getStyleForLabels());
+                }
+
+                // campos de texto para el formulario de añadidura de salones
+                TextField nameOfHallField = new TextField();
+                TextField descriptionOfHallField = new TextField();
+                TextField ubicationOfHallField = new TextField();
+                TextField capacityOfHallField = new TextField();
+                TextField dimensionOfHallField = new TextField();
+                TextField cellphoneOfHallField = new TextField();
+                TextField priceOfHallField = new TextField();
+
+                for (TextField textField : new TextField[] {
+                                nameOfHallField, descriptionOfHallField, ubicationOfHallField, capacityOfHallField, dimensionOfHallField,cellphoneOfHallField, priceOfHallField
+                }) {
+                        textField.setStyle(
+                                        "-fx-font-size: 13px; -fx-font-weight: 600; -fx-font-family: Cambria; -fx-border-radius: 6px; -fx-border-color:rgb(255, 255, 255); -fx-border-width: 3px; -fx-background-color: white;-fx-text-fill: black");
+                }
+
+                // boton para subir el salón
+                Button publicateButton = new Button("Publicar Salón");
+                publicateButton.setMaxWidth(200);
+                publicateButton.setMinHeight(30);
+                publicateButton.setStyle(
+                                "-fx-background-color: white; -fx-text-fill: black; -fx-font-size: 12px; -fx-font-family: Cambria;");
+
+                // grilla para ordenar los elementos
+                GridPane asideIzq = new GridPane();
+                asideIzq.add(nameOfHallLabel, 0, 0);
+                asideIzq.add(nameOfHallField, 1, 0);
+                asideIzq.add(descriptionOfHallLabel, 0, 1);
+                asideIzq.add(descriptionOfHallField, 1, 1);
+                asideIzq.add(ubicationOfHallLabel, 0, 2);
+                asideIzq.add(ubicationOfHallField, 1, 2);
+                asideIzq.add(capacityOfHallLabel, 0, 3);
+                asideIzq.add(capacityOfHallField, 1, 3);        
+                asideIzq.add(dimensionOfHallLabel, 0, 4);
+                asideIzq.add(dimensionOfHallField, 1, 4);
+                asideIzq.add(cellphoneOfHallLabel, 0, 5);
+                asideIzq.add(cellphoneOfHallField, 1, 5);
+                asideIzq.add(priceOfHallLabel, 0, 6);
+                asideIzq.add(priceOfHallField, 1, 6);
+                asideIzq.add(publicateButton, 0, 7);
+
+                // le damos gao y aliniacion alos elementos
+                GridPane.setColumnSpan(publicateButton, 2);
+                GridPane.setMargin(publicateButton, new Insets(30, 0, 0, 90));
+                asideIzq.setHgap(20);
+                asideIzq.setVgap(20);
+                asideIzq.setAlignment(Pos.CENTER);
+
+                // boton que nos permitira agregar la imagen
+                Button selectImage = new Button("", imageView);
+                selectImage.setMaxWidth(200);
+                selectImage.setMinHeight(200);
+                selectImage.setStyle(StylesConst.getStyleSelectHall());
+                Label info = new Label("Sube una imagen del salón a publicar");
+                info.setStyle("-fx-text-fill: white; -fx-font-size: 14px; -fx-font-family: Cambria; -fx-font-weight: 800;");
+
+                // contenedor para la imagen
+                VBox asideDer = new VBox();
+                asideDer.getChildren().addAll(selectImage, info);
+                asideDer.setAlignment(Pos.TOP_CENTER);
+
+                // margenes
+                HBox.setMargin(asideDer, new Insets(0, 0, 0, 50));
+                VBox.setMargin(selectImage, new Insets(157, 0, 20, 0));
+
+                // contenedor padre
+                HBox fatherContainer = new HBox();
+                fatherContainer.getChildren().addAll(asideIzq, asideDer);
+
+                DropShadow dropShadow = new DropShadow();
+                dropShadow.setRadius(10);
+                dropShadow.setOffsetX(4);
+                dropShadow.setOffsetY(7);
+                dropShadow.setColor(Color.GRAY);
+
+                // eventos del boton
+                publicateButton.setOnAction(e -> {
+                        PublicateHallController.comprobateInformation(nameOfHallField,descriptionOfHallField,ubicationOfHallField,capacityOfHallField,dimensionOfHallField,cellphoneOfHallField,priceOfHallField);
+                });
+
+                fatherContainer.setMaxWidth(700);
+                fatherContainer.setMinHeight(600);
+                fatherContainer
+                                .setStyle("-fx-background-color: linear-gradient(from 0% 0% to 100% 0%, #00e6f2, #6767eb, #2f00ff)");
+                fatherContainer.setEffect(dropShadow);
+
+                return fatherContainer;
         }
-
-        // campos de texto para el formulario de añadidura de salones
-        TextField nameOfHallField = new TextField();
-        TextField ubicationOfHallField = new TextField();
-        TextField priceOfHallField = new TextField();
-        TextField capacityOfHallField = new TextField();
-        TextField dimensionOfHallField = new TextField();
-
-        for (TextField textField : new TextField[] {
-                nameOfHallField, ubicationOfHallField, priceOfHallField, capacityOfHallField, dimensionOfHallField
-        }) {
-            textField.setStyle(
-                    "-fx-font-size: 13px; -fx-font-weight: 600; -fx-font-family: Cambria; -fx-border-radius: 6px; -fx-border-color:rgb(255, 255, 255); -fx-border-width: 3px; -fx-background-color: white;-fx-text-fill: black");
-        }
-
-        // boton para subir el salón
-        Button publicateButton = new Button("Publicar Salón");
-        publicateButton.setMaxWidth(200);
-        publicateButton.setMinHeight(30);
-        publicateButton.setStyle(
-                "-fx-background-color: white; -fx-text-fill: black; -fx-font-size: 12px; -fx-font-family: Cambria;");
-
-        // grilla para ordenar los elementos
-        GridPane asideIzq = new GridPane();
-        asideIzq.add(nameOfHallLabel, 0, 0);
-        asideIzq.add(nameOfHallField, 1, 0);
-        asideIzq.add(ubicationOfHallLabel, 0, 1);
-        asideIzq.add(ubicationOfHallField, 1, 1);
-        asideIzq.add(priceOfHallLabel, 0, 2);
-        asideIzq.add(priceOfHallField, 1, 2);
-        asideIzq.add(capacityOfHallLabel, 0, 3);
-        asideIzq.add(capacityOfHallField, 1, 3);
-        asideIzq.add(dimensionOfHallLabel, 0, 4);
-        asideIzq.add(dimensionOfHallField, 1, 4);
-        asideIzq.add(publicateButton, 0, 5);
-
-        // le damos gao y aliniacion alos elementos
-        GridPane.setColumnSpan(publicateButton, 2);
-        GridPane.setMargin(publicateButton, new Insets(30, 0, 0, 90));
-        asideIzq.setHgap(20);
-        asideIzq.setVgap(20);
-        asideIzq.setAlignment(Pos.CENTER);
-
-        // boton que nos permitira agregar la imagen
-        Button selectImage = new Button("", imageView);
-        selectImage.setMaxWidth(200);
-        selectImage.setMinHeight(200);
-        selectImage.setStyle(StylesConst.getStyleSelectHall());
-        Label info = new Label("Sube una imagen del salón a publicar");
-        info.setStyle("-fx-text-fill: white; -fx-font-size: 14px; -fx-font-family: Cambria; -fx-font-weight: 800;");
-
-        // contenedor para la imagen
-        VBox asideDer = new VBox();
-        asideDer.getChildren().addAll(selectImage, info);
-        asideDer.setAlignment(Pos.TOP_CENTER);
-
-        // margenes
-        HBox.setMargin(asideDer, new Insets(0, 0, 0, 50));
-        VBox.setMargin(selectImage, new Insets(157, 0, 20, 0));
-
-        // contenedor padre
-        HBox fatherContainer = new HBox();
-        fatherContainer.getChildren().addAll(asideIzq, asideDer);
-
-        DropShadow dropShadow = new DropShadow();
-        dropShadow.setRadius(10);
-        dropShadow.setOffsetX(4);
-        dropShadow.setOffsetY(7);
-        dropShadow.setColor(Color.GRAY);
-
-        fatherContainer.setMaxWidth(700);
-        fatherContainer.setMinHeight(600);
-        fatherContainer
-                .setStyle("-fx-background-color: linear-gradient(from 0% 0% to 100% 0%, #00e6f2, #6767eb, #2f00ff)");
-        fatherContainer.setEffect(dropShadow);
-
-        return fatherContainer;
-    }
 }
